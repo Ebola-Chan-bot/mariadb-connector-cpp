2c77686245212140637dfd746b6aa5860af0e30b
埃博拉酱 <vhtmfu@outlook.com>
2025-02-18 09:13:18 +0800

    v1.1.5.2

--- NuGet打包.ps1
+++ NuGet打包.ps1
@@ -1,4 +1,3 @@
 Copy-Item .\README.md .\NuGet
 Copy-Item .\include\* .\NuGet\build\native -Force -Recurse
-Copy-Item ..\mariadb-connector-c\include\* .\MariaDB_Connector -Force -Recurse
 nuget pack NuGet
